603d3edb7b687136ba2120e26a4bfc5b7b66c64a
李田所 <109799548+slqwqxd@users.noreply.github.com>
2022-09-18 11:27:32 +0800

    Add files via upload

---
+++ Projects/EDEGLESS CE/01-Components/00-Shell/main.bat
@@ -0,0 +1,71 @@
+rem Battery icon should be optional, skip now
+
+call AddFiles %0 :end_files
+goto :end_files
+
+; Computer Management shortcut
+; write menu command in registry directly
+; \ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Computer Management.lnk
+
+; Battery icon - In Winre.wim inf: hidbatt.inf,cmbatt.inf - drivers: battc.sys,HidBatt.sys,CmBatt.sys - system32: umpo.dll,umpnpmgr.dll
+\Windows\INF\battery.inf
+\Windows\INF\c_battery.inf
+@\Windows\System32\
+batmeter.dll
+
+@\Windows\System32\CatRoot\{F750E6C3-38EE-11D1-85E5-00C04FC295EE}\
++if "x%opt[shell.app]%"<>"xwinxshell"
++ver >= 17763
+Microsoft-Windows-Client-Desktop-Required-Package*.cat
+Microsoft-Windows-Client-Desktop-Required-WOW64-Package*.cat
++ver*
+Microsoft-Windows-Client-Features-Package*.cat
+Microsoft-Windows-Client-Features-WOW64-Package*.cat
+-if
+
+@\Windows\System32\
+
+; resources for Computer Management
+mycomput.dll
+
+; resources for This PC's Properties
+systemcpl.dll
+
+; resources for desktop background contextmenu
+Display.dll
+themecpl.dll
+
+; Microsoft FTP Folder
+msieftp.dll,shdocvw.dll
+
+; Add Network Location
+shwebsvc.dll
+
+; Create Shortcut Wizard
+;AppWiz.cpl
+;%WB_PE_LANG%\AppWiz.cpl.mui
+;osbaseln.dll
+
+; Details default folderview layout
+windows.storage.dll
+
+:end_files
+
+call RegCopy /-s "HKLM\SOFTWARE\Classes\*"
+
+rem Computer Management Command
+reg add HKLM\Tmp_software\Classes\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}\shell\Manage\command /ve /d "mmc.exe compmgmt.msc /s" /f
+
+rem fix blank shortcut icons
+reg add HKLM\Tmp_Software\Policies\Microsoft\Windows\Explorer /v EnableShellShortcutIconRemotePath /t REG_DWORD /d 1 /f
+
+rem Display, Personalize
+if not "x%opt[build.registry.software]%"=="xfull" (
+    call RegCopy HKLM\SOFTWARE\Classes\DesktopBackground\Shell
+)
+
+reg import Shell_RegDefault.reg
+reg import Shell_RegSoftware.reg
+
+call Explorer\submain.bat
+call WinXShell\submain.bat
---
+++ Projects/EDEGLESS CE/01-Components/00-Shell/za-StartMenu/StartIsBack/main.bat
@@ -0,0 +1,132 @@
+if "x%opt[shell.startmenu]%"=="xStartIsBack" goto :StartIsBackFiles
+if "x%opt[shell.startmenu]%"=="xStartAllBack" goto :StartAllBackFiles
+if not "x%opt[shell.startmenu]%"=="xStartXBack" goto :EOF
+
+set opt[shell.startmenu]=StartIsBack
+if %VER[3]% GEQ 22000 (
+    rem use StartAllBack for Windows 11
+    set opt[shell.startmenu]=StartAllBack
+    goto :StartAllBackFiles
+)
+
+:StartIsBackFiles
+set _startMenuName=StartIsBack
+call V2X StartIsBack -extract StartIsBackPlusPlus_setup[v%opt[SIB.version]%].exe "%X_PF%\StartIsBack"
+
+if not exist "%X%\Program Files\StartIsBack\StartIsBack64.dll" goto :EOF
+if not "%WB_PE_ARCH%"=="x64" del /f "%X%\Program Files\StartIsBack\StartIsBack64.dll"
+del /f "%X%\Program Files\StartIsBack\StartIsBackARM64.dll" 2> nul
+del /f "%X%\Program Files\StartIsBack\startscreen.exe" 2> nul
+del /f "%X%\Program Files\StartIsBack\UpdateCheck.exe" 2> nul
+goto :End_StartXBackFiles
+
+:StartAllBackFiles
+set _startMenuName=StartAllBack
+call V2X StartAllBack -extract StartAllBack_*_setup.exe "%X_PF%\StartAllBack"
+
+if not exist "%X_PF%\StartAllBack\StartAllBackX64.dll" goto :EOF
+del /f "%X%\Program Files\StartAllBack\StartAllBackLoaderA64.dll" 2> nul
+del /f "%X%\Program Files\StartAllBack\StartAllBackA64.dll" 2> nul
+del /f "%X%\Program Files\StartAllBack\UpdateCheck.exe" 2> nul
+:End_StartXBackFiles
+
+rem Support SIB v2.9.4
+reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell /v TabletMode /t REG_DWORD /d 0 /f
+
+rem call AddFiles %0 :end_files
+goto :end_files
+
+@\Windows\System32\
+authui.dll
+nrtapi.dll
+shdocvw.dll
+shutdownux.dll
+sud.dll
+van.dll
+
+:end_files
+
+if "%_startMenuName%"=="StartAllBack" (
+    reg import "%~dp0SAB_RegDefault.reg"
+    reg import "%~dp0SAB_RegSoftware.reg"
+) else (
+    reg import "%~dp0SIB_RegDefault.reg"
+    reg import "%~dp0SIB_RegSoftware.reg"
+)
+
+rem disable Win32 tray clock
+reg add HKLM\Tmp_SOFTWARE\Microsoft\Windows\CurrentVersion\ImmersiveShell /v UseWin32TrayClockExperience /t REG_DWORD /d 0 /f
+
+if "%WB_PE_ARCH%"=="x64" (
+    if not "x%opt[build.wow64support]%"=="xtrue" (
+        del /f "%X%\Program Files\StartIsBack\StartIsBack32.dll"
+        copy /y "%X_SYS%\regedt32.exe" "%X%\Program Files\%_startMenuName%\%_startMenuName%Cfg.exe"
+        (echo @start explorer.exe ::{26EE0668-A00A-44D7-9371-BEB064C98683}\0\::{BB06C0E4-D293-4F75-8A90-CB05B6477EEE}) > "%X%\Program Files\%_startMenuName%\%_startMenuName%CfgCmd.cmd"
+        reg add "HKLM\Tmp_Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%_startMenuName%Cfg.exe" /v Debugger /d "X:\Program Files\%_startMenuName%\%_startMenuName%CfgCmd.cmd" /f
+    )
+)
+
+if exist "%X_PF%\StartAllBack\StartAllBackCfgCmd.cmd" (
+    reg delete HKLM\Tmp_Software\Classes\sib-activate /f
+    reg delete HKLM\Tmp_Software\Classes\sib-reactivate /f
+)
+
+if "%_startMenuName%"=="StartAllBack" goto :update_config
+
+rem update dll path for x86
+if "%WB_PE_ARCH%"=="x64" goto :update_config
+call :update_clsid_dll_path {865e5e76-ad83-4dca-a109-50dc2113ce9b}
+call :update_clsid_dll_path {a2a9545d-a0c2-42b4-9708-a0b2badd77c9}
+call :update_clsid_dll_path {AD1405D2-30CF-4877-8468-1EE1C52C759F}
+call :update_clsid_dll_path {c71c41f1-ddad-42dc-a8fc-f5bfc61df958}
+call :update_clsid_dll_path {E5C31EC8-C5E6-4E07-957E-944DB4AAD85E}
+call :update_clsid_dll_path {FCEA18FF-BC55-4E63-94D7-1B2EFBFE706F}
+reg add HKLM\Tmp_Software\Classes\CLSID\{FCEA18FF-BC55-4E63-94D7-1B2EFBFE706F} /v LocalizedString /t REG_EXPAND_SZ /d @%^ProgramFiles^%\StartIsBack\StartIsBack32.dll,-510 /f
+
+:update_config
+if "x%opt[SIB.skin]%"=="x" set opt[SIB.skin]=Plain8
+set SIB_KEY=HKLM\Tmp_Default\Software\StartIsBack
+reg add %SIB_KEY% /v AlterStyle /d "X:\Program Files\%_startMenuName%\Styles\%opt[SIB.skin]%.msstyles" /f
+reg add %SIB_KEY% /v TaskbarStyle /d "X:\Program Files\%_startMenuName%\Styles\%opt[SIB.skin]%.msstyles" /f
+
+if "x%opt[SIB.skin]%"=="xPlain10" (
+    reg add %SIB_KEY% /v TaskbarStyle /d "X:\Program Files\%_startMenuName%\Styles\Windows 10.msstyles" /f
+)
+
+rem // StartIsBack Display as flyout menu (Windows XP style) value 0/1
+if not "x%opt[SIB.programs.flyout]%"=="xfalse" (
+    reg add %SIB_KEY% /v AllProgramsFlyout /t REG_DWORD /d 1 /f
+) else (
+    reg add %SIB_KEY% /v AllProgramsFlyout /t REG_DWORD /d 0 /f
+)
+
+rem // StartIsBack Start Menu Opaque
+if not "x%opt[SIB.style.opaque]%"=="xfalse" (
+    reg add %SIB_KEY% /v StartMenuColor  /t REG_DWORD /d 0xffffffff /f
+    reg add %SIB_KEY% /v StartMenuBlur   /t REG_DWORD /d 2 /f
+    reg add %SIB_KEY% /v StartMenuAlpha  /t REG_DWORD /d 255 /f
+    reg add %SIB_KEY% /v TaskbarColor  /t REG_DWORD /d 0xffffffff /f
+    reg add %SIB_KEY% /v TaskbarBlur   /t REG_DWORD /d 0 /f
+    reg add %SIB_KEY% /v TaskbarAlpha  /t REG_DWORD /d 255 /f
+)
+set SIB_KEY=
+
+rem Account Pictures
+set "_AccPic_Path=%X%\ProgramData\Microsoft\User Account Pictures"
+if not exist "%_AccPic_Path%\" mkdir "%_AccPic_Path%"
+rem copy /y "%WB_ROOT%\%APPDATA_DIR%\_CustomFiles_\user-200.png" "%_AccPic_Path%\"
+copy /y "%WB_ROOT%\%APPDATA_DIR%\_CustomFiles_\AccountPictures\*.*" "%_AccPic_Path%\"
+rem 2.9 and later
+reg add HKLM\Tmp_Software\Microsoft\Windows\CurrentVersion\PropertySystem\PropertyHandlers\.accountpicture-ms /ve /d {9a02e012-6303-4e1e-b9a1-630f802592c5} /f
+reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\AccountPicture /v SourceId /d user-200 /f
+call RegCopyEx Classes ".accountpicture-ms,accountpicturefile"
+set "_AccPic_Path=%X%\Users\Default\AppData\Roaming\Microsoft\Windows\AccountPictures"
+if not exist "%_AccPic_Path%\" mkdir "%_AccPic_Path%"
+copy /y "%WB_ROOT%\%APPDATA_DIR%\_CustomFiles_\AccountPictures\*.*" "%_AccPic_Path%\"
+set _AccPic_Path=
+
+set _startMenuName=
+goto :EOF
+
+:update_clsid_dll_path
+reg add HKLM\Tmp_Software\Classes\CLSID\%1\InProcServer32 /ve /d "X:\\Program Files\\StartIsBack\\StartIsBack32.dll" /f
