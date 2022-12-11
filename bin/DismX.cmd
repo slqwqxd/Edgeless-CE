0ecb468cd00e4b1b1f56c27e1f122c1a741e6928
李田所 <109799548+slqwqxd@users.noreply.github.com>
2022-12-11 19:52:24 +0800

    删除过时文件
    你干嘛,哈哈,诶呦

--- bin/DismX.cmd
+++
@@ -1,5 +0,0 @@
-if exist "%~dp0Dism10_x86\Dism.exe" (
-  "%~dp0Dism10_x86\Dism.exe" %*
-) else (
-  Dism.exe %*
-)
--- bin/cmdcolor_table.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-rem http://misc.flogisoft.com/bash/tip_colors_and_formatting#colors
-cscript //nologo cmdcolor_table.vbs|cmdcolor.exe
-pause
