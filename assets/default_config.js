0e9f130a64903777e9ff7046d8a1f01c52d00245
雫 <109799548+slqwqxd@users.noreply.github.com>
2022-12-11 22:01:46 +0800

    更新文件
    你干嘛,哈哈,诶呦

---
+++ assets/default_config.js
@@ -0,0 +1,55 @@
+var $width = 1000;
+var $height = 800;
+
+var $lang = ''; //auto
+
+var $app_auto_config_created = false;
+var $app_auto_save_window_size = true;
+
+var $ui_settings = {};
+$ui_settings['mode'] = 'Beginner';
+$ui_settings['lang'] = '';
+$ui_settings['enable_theme_loader'] = true;
+$ui_settings['theme'] = '';
+$ui_settings['dpi'] ='';
+$ui_settings['update_source'] = 'github';
+
+$ui_settings['custom_remote_url'] = 'https://';
+$ui_settings['custom_source_url'] = 'https://';
+
+if (eformat('x%APP_HOST_WIN%') != 'x10') {
+    $ui_settings['enable_theme_loader'] = false;
+}
+
+var $app_default_project = '';
+var $app_save_current_preset = true;
+var $wb_show_quick_build = false;
+
+var $wb_straight_mode = '1';
+
+var $wb_workspace = '.';
+var $wb_src_folder = '';
+var $wb_src = 'D:\\sources\\install.wim';
+var $wb_base = 'D:\\sources\\winre.wim';
+var $wb_auto_winre = true;
+var $wb_src_index = '1';
+var $wb_base_index = '1';
+
+var $wb_skip_project_page = false;
+
+var $wb_x_subst = true;
+var $wb_x_drv = 'auto';
+
+var $wb_waitfor_options = 2000;
+var $wb_waitfor_build = 2000;
+
+var $wb_auto_makeiso = false;
+var $wb_auto_testiso = false;
+var $wb_test_cmd = '';
+
+var $iso_boot_file = 'bootmgr';
+
+//skip update files' rights operation
+var $wb_skip_ufr = '1';
+//skip update registry's rights operation
+var $wb_skip_urr = '1';
